bdcad06fc0813c100504c4fa5e579c7ccfe5bb1c
Sean Betts <sean@Seans-iMac.home>
2020-07-18 15:54:48 +0100

    v1 commit

--- web-react/src/components/UserAccount/Dashboard/MemCount.js
+++ web-react/src/components/UserAccount/Dashboard/MemCount.js
@@ -21,7 +21,7 @@ const GET_COUNT_QUERY = gql`
   }
 `
 
-export default function Deposits() {
+export default function MemCount() {
     const classes = useStyles()
 
     const { loading, error, data } = useQuery(GET_COUNT_QUERY)
