c2ab8b6469ad4931ebdc214ab72df73c6dd76617
seanbetts <seanbetts@icloud.com>
2020-08-09 22:27:31 +0100

    fixed https

--- api/src/functions/graphql/graphql.js
+++ api/src/functions/graphql/graphql.js
@@ -10,7 +10,7 @@ const neo4j = require('neo4j-driver')
 const { typeDefs } = require('./graphql-schema')
 
 const driver = neo4j.driver(
-  process.env.NEO4J_URI || 'neo4j+s://graph.mems.life:7687',
+  process.env.NEO4J_URI || 'neo4j://graph.mems.life:7687',
   neo4j.auth.basic(
     process.env.NEO4J_USER || 'neo4j',
     process.env.NEO4J_PASSWORD || '72fofH0oav'
